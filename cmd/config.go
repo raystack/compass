2065f8c4a7618d97504f0e10582298a88f0ee900
Stewart Jingga <stewart_jingga@yahoo.com>
2021-04-06 20:22:32 +0700

    fix: config not being populated using env vars

--- cmd/config.go
+++ cmd/config.go
@@ -2,9 +2,10 @@ package cmd
 
 import (
 	"fmt"
-	"strings"
 
+	"github.com/jeremywohl/flatten"
 	"github.com/mcuadros/go-defaults"
+	"github.com/mitchellh/mapstructure"
 	"github.com/spf13/viper"
 )
 
@@ -33,7 +34,6 @@ func loadConfig() error {
 	viper.AddConfigPath("../")
 	viper.SetConfigType("yaml")
 	viper.AutomaticEnv()
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 
 	err := viper.ReadInConfig()
 	if err != nil {
@@ -44,12 +44,47 @@ func loadConfig() error {
 		}
 	}
 
+	bindEnvVars()
 	defaults.SetDefaults(&config)
-
 	err = viper.Unmarshal(&config)
 	if err != nil {
 		return fmt.Errorf("unable to unmarshal config to struct: %v\n", err)
 	}
 
 	return nil
+}
+
+func bindEnvVars() {
+	err, configKeys := getFlattenedStructKeys(Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	// Bind each conf fields to environment vars
+	for key := range configKeys {
+		err := viper.BindEnv(configKeys[key])
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func getFlattenedStructKeys(config Config) (error, []string) {
+	var structMap map[string]interface{}
+	err := mapstructure.Decode(config, &structMap)
+	if err != nil {
+		return err, nil
+	}
+
+	flat, err := flatten.Flatten(structMap, "", flatten.DotStyle)
+	if err != nil {
+		return err, nil
+	}
+
+	keys := make([]string, 0, len(flat))
+	for k := range flat {
+		keys = append(keys, k)
+	}
+
+	return nil, keys
 }
