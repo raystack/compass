d72ee2c8f947cd0ca3de0006f32261a936e19ecd
Stewart Jingga <stewart_jingga@yahoo.com>
2021-04-07 21:08:39 +0700

    feat: modify typehandler to not act as a router

--- api/router.go
+++ api/router.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
 	"github.com/odpf/columbus/api/handlers"
 	"github.com/odpf/columbus/models"
@@ -45,7 +47,54 @@ func setupRoutes(router *mux.Router, config Config) {
 	)
 
 	router.PathPrefix("/ping").Handler(handlers.NewHeartbeatHandler())
-	router.PathPrefix("/v1/types").Handler(typeHandler)
+	setupTypeRoutes(router, "/v1/types", typeHandler)
+
 	router.PathPrefix("/v1/search").Handler(searchHandler)
 	router.PathPrefix("/v1/lineage").Handler(lineageHandler)
+}
+
+func setupTypeRoutes(router *mux.Router, baseURL string, typeHandler *handlers.TypeHandler) {
+	router.Path(baseURL).
+		Methods(http.MethodGet).
+		HandlerFunc(typeHandler.GetAll)
+
+	// TODO: remove this route when
+	// getting type details already handled on GET baseUrl/{name}
+	router.Path(baseURL + "/{name}/details").
+		Methods(http.MethodGet).
+		HandlerFunc(typeHandler.GetType)
+
+	// TODO: switch this route to return type details
+	router.Path(baseURL+"/{name}").
+		Methods(http.MethodGet, http.MethodHead).
+		HandlerFunc(typeHandler.ListTypeRecords)
+
+	router.Path(baseURL+"/{name}/records").
+		Methods(http.MethodGet, http.MethodHead).
+		HandlerFunc(typeHandler.ListTypeRecords)
+
+	router.Path(baseURL).
+		Methods(http.MethodPut).
+		HandlerFunc(typeHandler.CreateOrReplaceType)
+
+	router.Path(baseURL + "/{name}").
+		Methods(http.MethodDelete).
+		HandlerFunc(typeHandler.DeleteType)
+
+	router.Path(baseURL + "/{name}/records/{id}").
+		Methods(http.MethodDelete).
+		HandlerFunc(typeHandler.DeleteRecord)
+
+	router.Path(baseURL + "/{name}").
+		Methods(http.MethodPut).
+		HandlerFunc(typeHandler.IngestRecord)
+
+	router.Path(baseURL+"/{name}/records/{id}").
+		Methods(http.MethodGet, http.MethodHead).
+		HandlerFunc(typeHandler.GetTypeRecord)
+
+	// TODO: remove this once no more request is coming
+	router.Path(baseURL+"/{name}/{id}").
+		Methods(http.MethodGet, http.MethodHead).
+		HandlerFunc(typeHandler.GetTypeRecord)
 }
