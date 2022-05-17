167a541199db96c7b80f54927093830d6cec4e4e
Gaurav Kumar <gk74533@gmail.com>
2022-05-17 11:36:44 +0530

    feat(cmd): add asset list command and setup client

--- cmd/assets.go
+++ cmd/assets.go
@@ -1,6 +1,11 @@
 package cmd
 
 import (
+	"fmt"
+
+	compassv1beta1 "github.com/odpf/compass/api/proto/odpf/compass/v1beta1"
+	"github.com/odpf/salt/printer"
+
 	"github.com/MakeNowJust/heredoc"
 	"github.com/spf13/cobra"
 )
@@ -14,12 +19,55 @@ func assetsCommand() *cobra.Command {
 			"group:core": "true",
 		},
 		Example: heredoc.Doc(`
-			$ guardian asset list
-			$ guardian asset get
-			$ guardian asset delete
-			$ guardian asset post
+			$ compass asset list
+			$ compass asset get
+			$ compass asset delete
+			$ compass asset post
 		`),
 	}
+
+	cmd.AddCommand(listAllAssetsCommand())
+	return cmd
+}
+
+func listAllAssetsCommand() *cobra.Command {
+	var host, header string
+
+	cmd := &cobra.Command{
+		Use:   "list",
+		Short: "lists all assets",
+		Example: heredoc.Doc(`
+			$ compass asset list --host=<hostaddress> --header=<key>:<value>
+		`),
+		Annotations: map[string]string{
+			"action:core": "true",
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			spinner := printer.Spin("")
+			defer spinner.Stop()
+
+			client, cancel, err := createClient(cmd, host)
+			if err != nil {
+				return err
+			}
+			defer cancel()
+
+			ctx := setCtxHeader(cmd.Context(), header)
+			res, err := client.GetAllAssets(ctx, &compassv1beta1.GetAllAssetsRequest{})
+			if err != nil {
+				return err
+			}
+
+			fmt.Println(res.GetData())
+
+			return nil
+		},
+	}
+
+	cmd.Flags().StringVarP(&header, "header", "H", "", "Header <key>:<value>")
+	cmd.MarkFlagRequired("header")
+	cmd.Flags().StringVarP(&host, "host", "h", "", "Guardian service to connect to")
+	cmd.MarkFlagRequired("host")
 
 	return cmd
 }
---
+++ cmd/client.go
@@ -0,0 +1,44 @@
+package cmd
+
+import (
+	"context"
+	"time"
+
+	compassv1beta1 "github.com/odpf/compass/api/proto/odpf/compass/v1beta1"
+	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
+)
+
+func createConnection(ctx context.Context, host string) (*grpc.ClientConn, error) {
+	opts := []grpc.DialOption{
+		grpc.WithInsecure(),
+		grpc.WithBlock(),
+	}
+
+	return grpc.DialContext(ctx, host, opts...)
+}
+
+func createClient(cmd *cobra.Command, host string) (compassv1beta1.CompassServiceClient, func(), error) {
+	// host, err := cmd.Flags().GetString("host")
+	// if err != nil {
+	// 	return nil, nil, err
+	// }
+	// if host == "" {
+	// 	return nil, nil, errors.New("\"host\" not set")
+	// }
+
+	dialTimeoutCtx, dialCancel := context.WithTimeout(cmd.Context(), time.Second*2)
+	conn, err := createConnection(dialTimeoutCtx, host)
+	if err != nil {
+		dialCancel()
+		return nil, nil, err
+	}
+
+	cancel := func() {
+		dialCancel()
+		conn.Close()
+	}
+
+	client := compassv1beta1.NewCompassServiceClient(conn)
+	return client, cancel, nil
+}
