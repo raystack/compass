d4738f38bd537a4ea203d43b43098c4abef0eed4
Muhammad Abduh <mabdh@users.noreply.github.com>
2022-05-19 14:13:06 +0700

    chore: add more tests to statsd monitor

--- pkg/metrics/statsd_monitor_test.go
+++ pkg/metrics/statsd_monitor_test.go
@@ -13,7 +13,7 @@ func TestNewStatsDMonitor(t *testing.T) {
 		metricsSeparator = "."
 	)
 
-	t.Run("MonitorLineage", func(t *testing.T) {
+	t.Run("Duration", func(t *testing.T) {
 		operationName := "build"
 		duration := 100
 		statsdClient := new(mocks.StatsDClient)
@@ -21,6 +21,56 @@ func TestNewStatsDMonitor(t *testing.T) {
 
 		monitor := metrics.NewStatsDMonitor(statsdClient, statsdPrefix, metricsSeparator)
 		monitor.Duration(operationName, duration)
+
+		statsdClient.AssertExpectations(t)
+	})
+
+	t.Run("ResponseTime", func(t *testing.T) {
+		requestMethod := "somemethod"
+		responseTime := int64(100)
+		requestURL := "requesturl"
+		statsdClient := new(mocks.StatsDClient)
+		statsdClient.EXPECT().Timing("compassApi.responseTime,method=somemethod,url=requesturl", responseTime).Once()
+
+		monitor := metrics.NewStatsDMonitor(statsdClient, statsdPrefix, metricsSeparator)
+		monitor.ResponseTime(requestMethod, requestURL, responseTime)
+
+		statsdClient.AssertExpectations(t)
+	})
+
+	t.Run("ResponseStatus", func(t *testing.T) {
+		requestMethod := "somemethod"
+		responseStatusCode := 200
+		requestURL := "requesturl"
+		statsdClient := new(mocks.StatsDClient)
+		statsdClient.EXPECT().Increment("compassApi.responseStatusCode,statusCode=200,method=somemethod,url=requesturl").Once()
+
+		monitor := metrics.NewStatsDMonitor(statsdClient, statsdPrefix, metricsSeparator)
+		monitor.ResponseStatus(requestMethod, requestURL, responseStatusCode)
+
+		statsdClient.AssertExpectations(t)
+	})
+
+	t.Run("ResponseTimeGRPC", func(t *testing.T) {
+		fullMethod := "somemethod/grpc"
+		responseTime := int64(100)
+		statsdClient := new(mocks.StatsDClient)
+		statsdClient.EXPECT().Timing("compassApi.responseTime,method=somemethod/grpc", responseTime).Once()
+
+		monitor := metrics.NewStatsDMonitor(statsdClient, statsdPrefix, metricsSeparator)
+		monitor.ResponseTimeGRPC(fullMethod, responseTime)
+
+		statsdClient.AssertExpectations(t)
+	})
+
+	t.Run("ResponseStatusGRPC", func(t *testing.T) {
+		fullMethod := "somemethod/grpc"
+		responseStatusCode := "OK"
+		statsdClient := new(mocks.StatsDClient)
+		statsdClient.EXPECT().Increment("compassApi.responseStatusCode,statusCode=OK,method=somemethod/grpc").Once()
+
+		monitor := metrics.NewStatsDMonitor(statsdClient, statsdPrefix, metricsSeparator)
+		monitor.ResponseStatusGRPC(fullMethod, responseStatusCode)
 
 		statsdClient.AssertExpectations(t)
 	})
