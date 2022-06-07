63eb861ff0e7078f0ce030ba2613cfcd227b3105
Muhammad Abduh <mabdh@users.noreply.github.com>
2022-06-07 15:55:57 +0700

    fix(compass): remove type whitelist

--- internal/server/v1beta1/search.go
+++ internal/server/v1beta1/search.go
@@ -11,10 +11,6 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-const (
-	whiteListQueryParamKey = "type"
-)
-
 func (server *APIServer) SearchAssets(ctx context.Context, req *compassv1beta1.SearchAssetsRequest) (*compassv1beta1.SearchAssetsResponse, error) {
 	_, err := server.validateUserInCtx(ctx)
 	if err != nil {
@@ -27,12 +23,11 @@ func (server *APIServer) SearchAssets(ctx context.Context, req *compassv1beta1.S
 	}
 
 	cfg := asset.SearchConfig{
-		Text:          text,
-		MaxResults:    int(req.GetSize()),
-		Filters:       filterConfigFromValues(req.GetFilter()),
-		RankBy:        req.GetRankby(),
-		Queries:       req.GetQuery(),
-		TypeWhiteList: parseTypeWhiteList(req.GetFilter()),
+		Text:       text,
+		MaxResults: int(req.GetSize()),
+		Filters:    filterConfigFromValues(req.GetFilter()),
+		RankBy:     req.GetRankby(),
+		Queries:    req.GetQuery(),
 	}
 
 	results, err := server.assetService.SearchAssets(ctx, cfg)
@@ -82,23 +77,10 @@ func (server *APIServer) SuggestAssets(ctx context.Context, req *compassv1beta1.
 func filterConfigFromValues(fltMap map[string]string) map[string][]string {
 	var filter = make(map[string][]string)
 	for key, value := range fltMap {
-		// filters are of form "filter[{field}]", apart from "filter[type]", which is used
-		// for building the type whitelist.
-		if key == whiteListQueryParamKey {
-			continue
-		}
-
 		var filterValues []string
 		filterValues = append(filterValues, strings.Split(value, ",")...)
 
 		filter[key] = filterValues
 	}
 	return filter
-}
-
-func parseTypeWhiteList(fltMap map[string]string) (types []string) {
-	if val, ok := fltMap[whiteListQueryParamKey]; ok {
-		types = append(types, strings.Split(val, ",")...)
-	}
-	return
 }
