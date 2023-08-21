e83ac41a026bd813d7c9e5eb92c2ba664dc64386
Suhas Karanth <sudo-suhas@users.noreply.github.com>
2023-08-21 12:16:50 +0530

    perf: index asset_urn, timestamp cols in asset_probes tbl (#55)

---
+++ internal/store/postgres/migrations/000017_create_idx_asset_probes_ts_table.up.sql
@@ -0,0 +1,1 @@
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_asset_probes_ts ON asset_probes (timestamp);
---
+++ internal/store/postgres/migrations/000018_create_idx_asset_probes_urn_table.up.sql
@@ -0,0 +1,1 @@
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_asset_probes_urn ON asset_probes (asset_urn);
